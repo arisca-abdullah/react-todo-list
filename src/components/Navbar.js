c1a9e42058ad16739bc587d4e9fb9b5be34a9539
Aris <arisca.abdullah08@gmail.com>
2019-10-18 20:05:35 +0700

    Create about page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 export default () => (
   <Navbar bg="dark" variant="dark">
@@ -8,6 +8,14 @@ export default () => (
       <Link className="navbar-brand" to="/">
         Todo List
       </Link>
+      <Nav className="mr-auto">
+        <Link className="nav-item nav-link" to="/">
+          Home
+        </Link>
+        <Link className="nav-item nav-link" to="/about">
+          About
+        </Link>
+      </Nav>
     </Container>
   </Navbar>
 );
