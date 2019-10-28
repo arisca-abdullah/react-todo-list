3d25e8433d3cdbac4776dfaaa6fadef0fb8614fc
Aris <arisca.abdullah08@gmail.com>
2019-10-28 15:00:16 +0700

    Updates

---
+++ precache-manifest.dde5065e3a326e7c60d9ec7bedbfb5ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90bc0c6c011110a373cb0e3372f58544",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "aa33dbba7fb12804dc50",
+    "url": "/react-todo-list/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c7cad0b74383b7c4e5e1",
+    "url": "/react-todo-list/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "aa33dbba7fb12804dc50",
+    "url": "/react-todo-list/static/js/2.bc783993.chunk.js"
+  },
+  {
+    "revision": "c7cad0b74383b7c4e5e1",
+    "url": "/react-todo-list/static/js/main.6bfbab36.chunk.js"
+  },
+  {
+    "revision": "b17ea6dcf128a617f03b",
+    "url": "/react-todo-list/static/js/runtime-main.68a283cc.js"
+  }
+]);
